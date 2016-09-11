1d134898f69c4b79d558b9e5279e2750c108f91a
pirozhenko Alexandr <pirog_13@ukr.net>
2016-09-11 23:09:16 +0300

    second rel

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,34 +1,51 @@
 angular.module("myDiagramm", ["chart.js"])
 .controller("LineCtrl", function ($scope, $http) {
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.labels = [];
   $scope.series = ['Series A', 'Series B'];
   $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
+    [],
+    []
   ];
+  $scope.regexp = /\d+/g;
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
+ 	$scope.showHide = false;
+ 	$scope.show = function() {
+  	$scope.showHide = !$scope.showHide;
+  };
 
+  // var data = {
+  //  {x: 50, y: 50}
+  // };
+  // var config = {
+  //    header: {
+  //      'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==',
+  //      'Access-Control-Allow-Origin': '*',
+    //    'Content-Type': 'application/json'
+    //    }
+  // };
+  $http.get('diagrams.json')
+    .success(function(res) {
+      res.forEach(function(item) {
+        $scope.coord = item.data;
+        $scope.data[0].push($scope.coord.x)
+        $scope.data[1].push($scope.coord.y)
+        $scope.label = item.label;
 
-  var config = {
-  		headers: {
-  			'Access-Control-Allow-Origin': '*',
-				'Content-Type': 'application/json'
-		  	},
-		  	data: {
-		  		'x': 1,
-		  		'y': 2
-		  	}
-  }
-  $http.get('http://testtask.callway.com.ua/Help/Api/GET-api-chart', config)
-  	.success(function(data, status, headers, config) {
-  			console.log('yes');
-  	})
-  	.error(function(data, status, headers, config){
-  			console.log(config);
-  	});
+        $scope.labels.push($scope.label)
+      })
+    });
+
+  $scope.addItem = function() {
+    $scope.data[0].push($scope.coordX);
+    $scope.data[1].push($scope.coordY);
+    $scope.labels.push($scope.newLabel);
+    $scope.coordX = '';
+    $scope.coordY = '';
+    $scope.newLabel = '';
+  };
 
   $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
   $scope.options = {
